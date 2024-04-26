f18550a5e348d650a16ce19a8da6e5674c6fb15f
Yaşar İdikut <yasar8000@gmail.com>
2024-04-25 20:55:36 -0400

    add changes

--- demo_no_algo.py
+++ demo_no_algo.py
@@ -21,6 +21,8 @@ fs = 100.0          # IMU sample frequency
 fs_gps = 10.0       # GPS sample frequency
 fs_mag = fs         # magnetometer sample frequency, not used for now
 
+motion_def_name = "motion_def-0to100"
+
 def test_path_gen():
     '''
     test only path generation in Sim.
@@ -32,7 +34,7 @@ def test_path_gen():
 
     #### start simulation
     sim = ins_sim.Sim([fs, fs_gps, fs_mag],
-                      motion_def_path+"//motion_def-3d.csv",
+                      os.path.join(motion_def_path, f"{motion_def_name}.csv"),
                       ref_frame=1,
                       imu=imu,
                       mode=None,
@@ -41,6 +43,34 @@ def test_path_gen():
     sim.run(1)
     # save simulation data to files
     sim.results('')
+    acc = sim.dmgr.get_data(["accel"])[0][0]
+    print(f"{acc.shape=}")
+
+    gyro = sim.dmgr.get_data(["gyro"])[0][0]
+    print(f"{gyro.shape=}")
+
+    pos = sim.dmgr.get_data(["ref_pos"])[0]
+    print(f"{pos.shape=} {pos[:, 2].min()=} {pos[:, 2].max()=}")
+
+    rot = sim.dmgr.get_data(["ref_att_euler"])[0]
+    print(f"{rot.shape=}")
+
+    time = sim.dmgr.get_data(["time"])[0]
+    print(f"{time.shape=}")
+
+    output_folder = os.path.abspath(f"output")
+    os.makedirs(output_folder, exist_ok=True)
+    with open(os.path.join(output_folder, f"{motion_def_name.lstrip('motion_def-')}.csv"), "w") as f:
+        f.write(f"ax (m/s^2), ay (m/s^2), az (m/s^2), gx (deg/s), gy (deg/s), gz (deg/s), ")
+        f.write(f"px (m), py (m), pz (m), rx (roll-deg), ry (pitch-deg), rz (yaw-deg), t (sec)\n")
+        for i in range(acc.shape[0]):
+            ax, ay, az = acc[i, 0], acc[i, 1], acc[i, 2]
+            gx, gy, gz = gyro[i, 0], gyro[i, 1], gyro[i, 2]
+            px, py, pz = pos[i, 0] - pos[max(0, i-1), 0], pos[i, 1] - pos[max(0, i-1), 1], pos[i, 2] - pos[max(0, i-1), 2]
+            rx, ry, rz = rot[i, 2] - rot[max(0, i-1), 2], rot[i, 1] - rot[max(0, i-1), 1], rot[i, 0] - rot[max(0, i-1), 0]
+            t = time[i]
+            f.write(f"{ax}, {ay}, {az}, {gx}, {gy}, {gz}, {px}, {py}, {pz}, {rx}, {ry}, {rz}, {t}\n")
+
     # plot data, 3d plot of reference positoin, 2d plots of gyro and accel
     sim.plot(['ref_pos', 'gyro', 'gps_visibility'], opt={'ref_pos': '3d'})
 
